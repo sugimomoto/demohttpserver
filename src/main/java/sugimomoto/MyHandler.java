f4d521a09a543688227151994dede398e3f03b7d
sugimomoto <sugimomoto@gmail.com>
2022-03-03 08:00:55 +0900

    Added to get authorizationCode and state

--- src/main/java/sugimomoto/MyHandler.java
+++ src/main/java/sugimomoto/MyHandler.java
@@ -3,6 +3,11 @@ package sugimomoto;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
@@ -11,7 +16,6 @@ import com.sun.net.httpserver.HttpServer;
 
 
 public class MyHandler implements HttpHandler {
-
 
     /*
     http://localhost:33333/?code=XXXXX&state=12345
@@ -22,12 +26,30 @@ public class MyHandler implements HttpHandler {
 
         String response =  "Your request " + exchange.getRequestURI();
 
-        String authorizationCode = (String)exchange.getAttribute("code");
-        String state = (String)exchange.getAttribute("state");
+        Map<String, String> params = getParamMap(exchange.getRequestURI().getQuery());
+
+        String authorizationCode = params.get("code");
+        String state = params.get("state");
 
         exchange.sendResponseHeaders(200, response.length());
         OutputStream os = exchange.getResponseBody();
         os.write(response.getBytes());
         os.close();
+    }
+
+    private Map<String, String> getParamMap(String query) {
+
+        if(query == null || query.isBlank()){
+            return Collections.emptyMap();
+        }
+
+        Map<String,String> results = new HashMap<>();
+
+        for(String param : query.split("&")){
+            String[] entry = param.split("=");
+            results.put(entry[0], entry[1]);
+        }
+
+        return results;
     }
 }
